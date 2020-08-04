1ad258925eea31a36de4c0b0e3e2e959f368bb06
Joko Eliyanto <63438444+eliyanto29@users.noreply.github.com>
2020-08-05 05:52:12 +0700

    Add files via upload

---
+++ 1_Tipe_Variabel.R
@@ -0,0 +1,41 @@
+#================= R for Data Science =================#
+#Series No: 1
+#Title    : Tipe Variabel
+#======================================================#
+
+#Author         : Joko Eliyanto
+#Youtube Channel: Hobi Data
+#Github         : eliyanto29
+#Instagram      : @hobi_data
+#Facebook       : Hobi Data
+
+#======================================================#
+
+
+#Note: Bilangan Bulat/Integer
+x=2L
+typeof(x)
+
+#Note: Bilangan Real/Double
+y=2
+typeof(y)
+
+#Note: Bilangan Kompleks/Complex
+# z=a+bi,  i=akar(-1)
+
+z=2+3i
+typeof(z)
+
+#Note: Karakter/Huruf/String
+#pakai tanda petik ("")
+a="Nama"
+typeof(a)
+
+#Note: Logical/Nilai Kebenaran Proposisi
+b=FALSE
+typeof(b)
+
+
+
+
+
